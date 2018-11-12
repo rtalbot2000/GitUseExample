d4cefb270d5fd8d515e5955aff7859d6b63f0da9
Ryan Talbot <Ryan.Talbot.829@k12.friscoisd.org>
2018-11-12 14:19:03 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Ryan Talbot on 11/12/18.
+//  Copyright © 2018 Ryan Talbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
