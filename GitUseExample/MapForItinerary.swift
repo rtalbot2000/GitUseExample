e81d12b0212cf7a76e9f82b10603a72e04498b7e
Ryan Talbot <Ryan.Talbot.829@k12.friscoisd.org>
2018-11-14 13:23:42 -0600

    Added new map for itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,15 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Ryan Talbot on 11/14/18.
+//  Copyright © 2018 Ryan Talbot. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary : NSObject {
+    
+    
+    
+}
